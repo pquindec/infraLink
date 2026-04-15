0d104d344da353a1cdc81c21c50f47969d9059e1
Paúl Quinde <paquinde@netlife.net.ec>
2026-04-15 10:48:00 -0500

    Cambio de logotipo

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,23 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = () => {
+  const site = 'https://www.infralinknet.com';
+  const today = new Date().toISOString().split('T')[0];
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${site}/</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+      'Cache-Control': 'public, max-age=3600',
+    },
+  });
+};
